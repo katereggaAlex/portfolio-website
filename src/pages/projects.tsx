57342cfd6123ec2137338adc24daf0e1e27ac591
kabba joseph timothy <kabbajoseph072@gmail.com>
2022-10-08 22:40:54 +0300

    done

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,6 +1,22 @@
 import { navLinks } from "./nav/nav_links";
+import data from "../data/personal.json";
+import { useState } from "react";
+import { Project } from "../projects/projects";
 
 function Projects() {
+  const [projects] = useState(() => {
+    const featured: Project[] = data.projects.filter(
+      (value) => value.featured === true
+    );
+    const silent: Project[] = data.projects.filter(
+      (value) => value.featured === false
+    );
+
+    return {
+      featured: featured,
+      silent: silent,
+    };
+  });
   return (
     <div id={navLinks.project.destination} className="layer">
       <h1 className="heading">Projects</h1>
